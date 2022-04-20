c67c7233ff259bdbf6758e378705acba599b3e87
1973-clinton <clintonboamahofficial@gmail.com>
2022-04-20 21:30:53 +0000

    Add project files.

---
+++ FindDistinctValues/Logic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindDistinctValues
+{
+    public static class Logic
+    {
+        public static List<string> GetDistinct(List<string> input)
+        {
+            return input.Distinct().ToList();
+        }           
+
+    }
+}
